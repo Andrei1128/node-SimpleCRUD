2b2c75f33a8f4971acbc7a6ba793f1461a0f98a0
Andrei <nicolaev.andrei09@gmail.com>
2023-03-02 21:33:58 +0200

    commit 4

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const ejsMate = require("ejs-mate");
-const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
-const { v4: uuidv4 } = require("uuid");
-const PORT = 3000;
-
-let produse = [
-  {
-    id: "1",
-    nume: "banana",
-    cantitate: 5,
-  },
-  {
-    id: "2",
-    nume: "portocale",
-    cantitate: 3,
-  },
-  {
-    id: "3",
-    nume: "mere",
-    cantitate: 10,
-  },
-];
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+mongoose.connect(MONGO_URL).catch((error) => console.log(error));
+const ProdusSchema = new mongoose.Schema({
+  nume: { type: String, required: true },
+  cantitate: { type: Number, required: true },
+});
+const produs = mongoose.model("produs", ProdusSchema);
 
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
@@ -37,35 +26,29 @@ app.get("/", (req, res) => {
   res.redirect("/produse");
 });
 
-app.get("/produse", (req, res) => {
+app.get("/produse", async (req, res) => {
+  const produse = await produs.find({});
   res.render("produs/index.ejs", { produse });
 });
 
-app.get("/produse/:id", (req, res) => {
-  const produs = produse.find((produs) => produs.id === req.params.id);
-  res.render("produs/produs.ejs", { produs });
+app.get("/produse/:id", async (req, res) => {
+  const produsGasit = await produs.findById(req.params.id);
+  res.render("produs/produs.ejs", { produs: produsGasit });
 });
 
-app.delete("/produse/:id", (req, res) => {
-  produse = produse.filter((produse) => produse.id !== req.params.id);
+app.delete("/produse/:id", async (req, res) => {
+  await produs.findByIdAndDelete(req.params.id);
   res.redirect("/produse");
 });
 
-app.put("/produse/:id", (req, res) => {
-  var produs = produse.find((produs) => produs.id === req.params.id);
-  produs.nume = req.body.nume;
-  produs.cantitate = req.body.cantitate;
+app.put("/produse/:id", async (req, res) => {
+  await produs.findByIdAndUpdate(req.params.id, req.body);
   res.redirect("/produse");
 });
 
-app.post("/produse", (req, res) => {
-  produse = [
-    ...produse,
-    {
-      ...req.body,
-      id: uuidv4(),
-    },
-  ];
+app.post("/produse", async (req, res) => {
+  const produsNou = new produs(req.body);
+  await produsNou.save();
   res.redirect("/produse");
 });
 
